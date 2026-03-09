581ebe4bc836b6c5d7c42e5c02729c31980ad72f
Andrew <akariaka@yandex.ru>
2026-03-09 20:54:14 +0600

    install nodemon

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('test');
+import { createServer } from 'node:http';
+
+const port = 8001;
+
+const server = createServer();
+
+server.on('request', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.end('portal')
+})
+
+server.listen(port)
