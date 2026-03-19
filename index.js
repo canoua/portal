ab64ab0b2aeb6eb4867418cf8c823b0b724e2488
Andrew <akariaka@yandex.ru>
2026-03-19 23:16:31 +0600

    add tasks, styles

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import { config } from 'dotenv';
-import router from './source/controllers/router.js';
+import express from "express";
+import { config } from "dotenv";
+import router from "./source/controllers/router.js";
 
 config();
 
@@ -8,11 +8,14 @@ const port = process.env.PORT || 8002;
 const app = express();
 
 // для использования переменно из .env в хэдере
-app.locals.appTitle = process.env.APPTITLE || 'Express';
+app.locals.appTitle = process.env.APPTITLE || "Express";
+
+// для стилей
+app.use(express.static("public"));
 
 // для использования ejs
-app.set('view engine', 'ejs');
-app.set('views', './source/templates');
+app.set("view engine", "ejs");
+app.set("views", "./source/templates");
 
-app.use('/', router);
+app.use("/", router);
 app.listen(port);
